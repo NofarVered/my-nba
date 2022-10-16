a6da6d9bc509b1435f6960bf34cb9562c1c4d91a
NofarVered <nofarss79@gmail.com>
2022-10-16 17:20:37 +0300

    active player

--- backend/routers/utils/data_nba_api_functions.py
+++ backend/routers/utils/data_nba_api_functions.py
@@ -7,10 +7,12 @@ def filter_by_team(players_response, teamName, isActive):
     result = [Player(**curr_player, img=IMG_URL % (
         curr_player["lastName"], curr_player["firstName"])) for curr_player in players_response if curr_player["teamId"] == teams_id[teamName]]
     if (isActive):
-        result = [Player(**curr_player, img=IMG_URL % (
-            curr_player["lastName"], curr_player["firstName"])) for curr_player in players_response if curr_player["teamId"] == teams_id[teamName] and curr_player["isActive"] == True]
+        result = [
+            curr_player for curr_player in result if curr_player["isActive"] == True]
     return result
 
 
 def filter_player_stats(stats_response):
+    # TO DO
     result = []
+    return result
