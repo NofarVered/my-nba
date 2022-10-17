f4c5bb1496f34eb424d7bff6235f88914d287953
NofarVered <nofarss79@gmail.com>
2022-10-18 02:35:39 +0300

    data to be complite....

--- frontend/src/app.data.ts
+++ frontend/src/app.data.ts
@@ -2,81 +2,81 @@ class Data{
     static currentTeam:Team
     static dreamTeam:Team
 
-    get players(){
-        return [...this._players];
-    }
+    // get players(){
+    //     return [...this._players];
+    // }
 
-    addPlayer(player: Player): void{
-        let existPlayer: Player|undefined = this._players.find(p => p.id == player.id)
-        if (!existPlayer){
-            this._players.push(player);
-        }
-    }
+    // addPlayer(player: Player): void{
+    //     let existPlayer: Player|undefined = this._players.find(p => p.id == player.id)
+    //     if (!existPlayer){
+    //         this._players.push(player);
+    //     }
+    // }
 
-    getPlayerById(playerId: number): Player|undefined{
-        let player: Player|undefined = this._players.find(p => p.id == playerId)
-        return player;
-    }
+    // getPlayerById(playerId: number): Player|undefined{
+    //     let player: Player|undefined = this._players.find(p => p.id == playerId)
+    //     return player;
+    // }
 
-    removePlayerById(playerId: number): void{
-        for(let i = 0; i < this._players.length; i++){
-            let curPlayer = this._players[i]
-            if(curPlayer.id == playerId){
-                this._players.splice(i, 1);
-            }
-        }
-    }
+    // removePlayerById(playerId: number): void{
+    //     for(let i = 0; i < this._players.length; i++){
+    //         let curPlayer = this._players[i]
+    //         if(curPlayer.id == playerId){
+    //             this._players.splice(i, 1);
+    //         }
+    //     }
+    // }
 
-    async generateStatsOfPlayer(playerId: number){
-        let player = this.getPlayerById(playerId)
-        if (player !== undefined){
-            let firstName: string = player.firstName;
-            let lastName: string = player.lastName;
-            let stats = await $.get(`/players/stats/${firstName}/${lastName}`);
-            return stats;
-        }
-    }
+    // async generateStatsOfPlayer(playerId: number){
+    //     let player = this.getPlayerById(playerId)
+    //     if (player !== undefined){
+    //         let firstName: string = player.firstName;
+    //         let lastName: string = player.lastName;
+    //         let stats = await $.get(`/players/stats/${firstName}/${lastName}`);
+    //         return stats;
+    //     }
+    // }
 
-    async generatePlayers(teamName: string, year: number, filterByHasBitrhDate: boolean){
-        let dataPlayers = await $.get(`/players?teamName=${teamName}&year=${year}&hasBirthDate=${filterByHasBitrhDate}`);
-        this._players.splice(0);
-        for(const player of dataPlayers["players"]){
-            const {personId, firstName, lastName, jersey, pos, picture} = player;
-            this.addPlayer(new Player(personId, firstName, lastName, jersey, pos, picture));
-        }
-    }
+    // async generatePlayers(teamName: string, year: number, filterByHasBitrhDate: boolean){
+    //     let dataPlayers = await $.get(`/players?teamName=${teamName}&year=${year}&hasBirthDate=${filterByHasBitrhDate}`);
+    //     this._players.splice(0);
+    //     for(const player of dataPlayers["players"]){
+    //         const {personId, firstName, lastName, jersey, pos, picture} = player;
+    //         this.addPlayer(new Player(personId, firstName, lastName, jersey, pos, picture));
+    //     }
+    // }
 
-    async generateDreamTeam(){
-        let dreamTeamPlayers = await $.get("/dreamTeam")
-        this._players.splice(0);
-        for(const player of dreamTeamPlayers["players"]){
-            const {personId, firstName, lastName, jersey, pos, picture} = player;
-            this.addPlayer(new Player(personId, firstName, lastName, jersey, pos, picture));
-        }
-    }
+    // async generateDreamTeam(){
+    //     let dreamTeamPlayers = await $.get("/dreamTeam")
+    //     this._players.splice(0);
+    //     for(const player of dreamTeamPlayers["players"]){
+    //         const {personId, firstName, lastName, jersey, pos, picture} = player;
+    //         this.addPlayer(new Player(personId, firstName, lastName, jersey, pos, picture));
+    //     }
+    // }
 
-    addPlayerToDreamTeam(playerId: number){
-        let player = this.getPlayerById(playerId);
-        if (player != undefined){
-            $.post("/dreamTeam", JSON.stringify({
-                "personId": player.id,
-                "firstName": player.firstName,
-                "lastName": player.lastName,
-                "jersey": player.jerseyNumber,
-                "pos": player.position,
-                "picture": player.picture
-            }))
-        }
-    }
+    // addPlayerToDreamTeam(playerId: number){
+    //     let player = this.getPlayerById(playerId);
+    //     if (player != undefined){
+    //         $.post("/dreamTeam", JSON.stringify({
+    //             "personId": player.id,
+    //             "firstName": player.firstName,
+    //             "lastName": player.lastName,
+    //             "jersey": player.jerseyNumber,
+    //             "pos": player.position,
+    //             "picture": player.picture
+    //         }))
+    //     }
+    // }
 
-    removePlayerFromDreamTeam(playerId: number){
-        let player = this.getPlayerById(playerId);
-        if(player != undefined){
-            this.removePlayerById(playerId)
-            return $.ajax({
-                url: `/dreamTeam/${playerId}`,
-                method: "DELETE"
-            })
-        }
-    }
+    // removePlayerFromDreamTeam(playerId: number){
+    //     let player = this.getPlayerById(playerId);
+    //     if(player != undefined){
+    //         this.removePlayerById(playerId)
+    //         return $.ajax({
+    //             url: `/dreamTeam/${playerId}`,
+    //             method: "DELETE"
+    //         })
+    //     }
+    // }
 }
